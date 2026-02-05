1eaa85715e17032ff9a7b61caf2941433c326dc6
AWAIS MAYO KING009 <rojxb268@gmail.com>
2026-02-05 12:23:20 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botName: "⚡ 𝙰𝚆𝙰𝙸𝚂 𝙼𝙰𝚈𝙾 𝙲𝚈𝙱𝙴𝚁 ⚡",
+    ownerName: "𝙰𝚆𝙰𝙸𝚂 𝙼𝙰𝚈𝙾",
+    ownerNumber: "923295533214", 
+    prefix: ".",
+    footer: "© 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 𝙰𝚠𝚊𝚒𝚜 𝙼𝚊𝚢𝚘 ☠️",
+    thumb: "./Awais.png" // آپ کی امیج کا نام
+};
